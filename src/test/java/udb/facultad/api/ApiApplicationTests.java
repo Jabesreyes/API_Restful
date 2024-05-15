dfd78f453af3f55547845e279c581170d8ea4a16
Jabesreyes <jabesrey@gmail.com>
2024-05-15 12:32:16 -0600

    first commit

---
+++ src/test/java/udb/facultad/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package udb.facultad.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
